b781a5cfa2033ed6749b4be204d36ae8301cd82b
Hisayori Noda <nodchip@gmail.com>
2015-10-07 21:36:09 +0900

    ベンチマークスクリプトを追加した

---
+++ src/benchmark_kpp_padding.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+OUTPUT_DIRECTORY=benchmark
+RESULT_FILE=${OUTPUT_DIRECTORY}/result.csv
+PATH_BASE=${PATH}
+rm -rf ${OUTPUT_DIRECTORY}
+mkdir -p ${OUTPUT_DIRECTORY}
+for kpp_padding0 in 0 1 2 3 4 5 6 7
+do 
+  for kpp_padding1 in 0 1 2 3 4 5 6 7
+  do 
+    echo ${kpp_padding0} ${kpp_padding1}
+    OUTPUT_FILE=${OUTPUT_DIRECTORY}/${kpp_padding0}-${kpp_padding1}.txt
+    g++ --version > ${OUTPUT_FILE}
+    KPP_PADDING0=${kpp_padding0} KPP_PADDING1=${kpp_padding1} make -e -j4 clean pgo >> ${OUTPUT_FILE}
+    echo ${kpp_padding0},${kpp_padding1},`tail -n 1 ${OUTPUT_FILE} | cut -f3 -d" "` >> ${RESULT_FILE}
+  done
+done
+
+make clean
