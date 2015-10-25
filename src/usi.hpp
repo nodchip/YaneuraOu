4bea4b429381ee27e55941a8c3473d380fd84e4b
Hisayori Noda <nodchip@gmail.com>
2015-10-25 10:21:15 +0900

    gccでコンパイルが通らないのを修正した

--- src/usi.hpp
+++ src/usi.hpp
@@ -11,32 +11,32 @@ struct OptionsMap;
 
 namespace OptionNames
 {
-  constexpr char* USI_HASH = "USI_Hash";
-  constexpr char* CLEAR_HASH = "Clear_Hash";
-  constexpr char* BOOK_FILE = "Book_File";
-  constexpr char* BEST_BOOK_MOVE = "Best_Book_Move";
-  constexpr char* OWNBOOK = "OwnBook";
-  constexpr char* MIN_BOOK_PLY = "Min_Book_Ply";
-  constexpr char* MAX_BOOK_PLY = "Max_Book_Ply";
-  constexpr char* MIN_BOOK_SCORE = "Min_Book_Score";
-  constexpr char* EVAL_DIR = "Eval_Dir";
-  constexpr char* WRITE_SYNTHESIZED_EVAL = "Write_Synthesized_Eval";
-  constexpr char* USI_PONDER = "USI_Ponder";
-  constexpr char* BYOYOMI_MARGIN = "Byoyomi_Margin";
-  constexpr char* PONDER_TIME_MARGIN = "Ponder_Time_Margin";
-  constexpr char* MULTIPV = "MultiPV";
-  constexpr char* SKILL_LEVEL = "Skill_Level";
-  constexpr char* MAX_RANDOM_SCORE_DIFF = "Max_Random_Score_Diff";
-  constexpr char* MAX_RANDOM_SCORE_DIFF_PLY = "Max_Random_Score_Diff_Ply";
-  constexpr char* EMERGENCY_MOVE_HORIZON = "Emergency_Move_Horizon";
-  constexpr char* EMERGENCY_BASE_TIME = "Emergency_Base_Time";
-  constexpr char* EMERGENCY_MOVE_TIME = "Emergency_Move_Time";
-  constexpr char* SLOW_MOVER = "Slow_Mover";
-  constexpr char* MINIMUM_THINKING_TIME = "Minimum_Thinking_Time";
-  constexpr char* MAX_THREADS_PER_SPLIT_POINT = "Max_Threads_per_Split_Point";
-  constexpr char* THREADS = "Threads";
-  constexpr char* USE_SLEEPING_THREADS = "Use_Sleeping_Threads";
-  constexpr char* DANGER_DEMERIT_SCORE = "Danger_Demerit_Score";
+  constexpr const char* USI_HASH = "USI_Hash";
+  constexpr const char* CLEAR_HASH = "Clear_Hash";
+  constexpr const char* BOOK_FILE = "Book_File";
+  constexpr const char* BEST_BOOK_MOVE = "Best_Book_Move";
+  constexpr const char* OWNBOOK = "OwnBook";
+  constexpr const char* MIN_BOOK_PLY = "Min_Book_Ply";
+  constexpr const char* MAX_BOOK_PLY = "Max_Book_Ply";
+  constexpr const char* MIN_BOOK_SCORE = "Min_Book_Score";
+  constexpr const char* EVAL_DIR = "Eval_Dir";
+  constexpr const char* WRITE_SYNTHESIZED_EVAL = "Write_Synthesized_Eval";
+  constexpr const char* USI_PONDER = "USI_Ponder";
+  constexpr const char* BYOYOMI_MARGIN = "Byoyomi_Margin";
+  constexpr const char* PONDER_TIME_MARGIN = "Ponder_Time_Margin";
+  constexpr const char* MULTIPV = "MultiPV";
+  constexpr const char* SKILL_LEVEL = "Skill_Level";
+  constexpr const char* MAX_RANDOM_SCORE_DIFF = "Max_Random_Score_Diff";
+  constexpr const char* MAX_RANDOM_SCORE_DIFF_PLY = "Max_Random_Score_Diff_Ply";
+  constexpr const char* EMERGENCY_MOVE_HORIZON = "Emergency_Move_Horizon";
+  constexpr const char* EMERGENCY_BASE_TIME = "Emergency_Base_Time";
+  constexpr const char* EMERGENCY_MOVE_TIME = "Emergency_Move_Time";
+  constexpr const char* SLOW_MOVER = "Slow_Mover";
+  constexpr const char* MINIMUM_THINKING_TIME = "Minimum_Thinking_Time";
+  constexpr const char* MAX_THREADS_PER_SPLIT_POINT = "Max_Threads_per_Split_Point";
+  constexpr const char* THREADS = "Threads";
+  constexpr const char* USE_SLEEPING_THREADS = "Use_Sleeping_Threads";
+  constexpr const char* DANGER_DEMERIT_SCORE = "Danger_Demerit_Score";
 }
 
 class USIOption {
