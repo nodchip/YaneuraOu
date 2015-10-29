44b2c1c154c852d6582d01fbee9a4f846a8802e1
Hisayori Noda <nodchip@gmail.com>
2015-10-29 19:22:33 +0900

    ベンチマークスクリプトで実行パラメーターを変更した

--- src/benchmark_cache_size.sh
+++ src/benchmark_cache_size.sh
@@ -4,19 +4,30 @@ RESULT_FILE=${OUTPUT_DIRECTORY}/result.csv
 PATH_BASE=${PATH}
 rm -rf ${OUTPUT_DIRECTORY}
 mkdir -p ${OUTPUT_DIRECTORY}
-for tt_size in "1024" "2048" "4096" "8192"
+for USI_HASH_FOR_BENCHMARK in "1024" "2048" "4096" "8192"
 do 
-  for eval_size in 0x8000000LL 0x10000000LL 0x20000000LL 0x40000000LL
+  for EVALUATE_TABLE_SIZE in 0x8000000LL 0x10000000LL 0x20000000LL 0x40000000LL
   do 
-    if [ ${tt_size} = "8192" ] && [ ${eval_size} = 0x40000000LL ]
+    if [ ${USI_HASH_FOR_BENCHMARK} = "8192" ] && [ ${EVALUATE_TABLE_SIZE} = 0x40000000LL ]
     then
       break
     fi
-    echo ${tt_size} ${eval_size}
-    OUTPUT_FILE=${OUTPUT_DIRECTORY}/${tt_size}-${eval_size}.txt
-    make USI_HASH_FOR_BENCHMARK=\"${tt_size}\" EVALUATE_TABLE_SIZE=${eval_size} -j4 clean pgo
+
+    if [ ${USI_HASH_FOR_BENCHMARK} = "8192" ] && [ ${EVALUATE_TABLE_SIZE} = 0x20000000LL ]
+    then
+      break
+    fi
+
+    if [ ${USI_HASH_FOR_BENCHMARK} = "4096" ] && [ ${EVALUATE_TABLE_SIZE} = 0x40000000LL ]
+    then
+      break
+    fi
+
+    echo ${USI_HASH_FOR_BENCHMARK} ${EVALUATE_TABLE_SIZE}
+    OUTPUT_FILE=${OUTPUT_DIRECTORY}/${USI_HASH_FOR_BENCHMARK}-${EVALUATE_TABLE_SIZE}.txt
+    make USI_HASH_FOR_BENCHMARK=\\\"${USI_HASH_FOR_BENCHMARK}\\\" EVALUATE_TABLE_SIZE=${EVALUATE_TABLE_SIZE} -j4 clean pgo
     ./tanuki-clang benchmark_elapsed_for_depth_n >> ${OUTPUT_FILE}
-    echo ${tt_size},${eval_size},`tail -n 1 ${OUTPUT_FILE} | cut -f5 -d" "` >> ${RESULT_FILE}
+    echo ${USI_HASH_FOR_BENCHMARK},${EVALUATE_TABLE_SIZE},`tail -n 1 ${OUTPUT_FILE} | cut -f5 -d" "` >> ${RESULT_FILE}
   done
 done
 
