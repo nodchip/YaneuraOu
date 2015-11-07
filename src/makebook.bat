25de693ed88cc9b86ad32a8e6b61db8806fb1b0b
Hisayori Noda <nodchip@gmail.com>
2015-11-07 11:05:58 +0900

    定石データベース生成スクリプトを追加した

---
+++ src/makebook.bat
@@ -0,0 +1,3 @@
+..\tanuki-\x64\Release\tanuki-.exe concat_csa_files
+..\tanuki-\x64\Release\tanuki-.exe merge_csa_files
+..\tanuki-\x64\Release\tanuki-.exe b "C:\home\develop\shogi-kifu\merged.csa1"
