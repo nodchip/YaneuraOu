098b98d21b8e00b7c8f1e94a632b49cd7d750219
Hisayori Noda <nodchip@gmail.com>
2015-11-17 18:52:54 +0900

    パラメーター調整

--- src/book.cpp
+++ src/book.cpp
@@ -364,7 +364,7 @@ void makeBook(Position& pos, std::istringstream& ssCmd) {
   }
 #endif
 
-  std::ofstream ofs("book-2015-11-14.bin", std::ios::binary);
+  std::ofstream ofs("book-2015-11-16.bin", std::ios::binary);
   for (auto& elem : bookMap) {
     for (auto& elel : elem.second) {
       ofs.write(reinterpret_cast<char*>(&(elel)), sizeof(BookEntry));
