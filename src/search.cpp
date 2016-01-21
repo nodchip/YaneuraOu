86cc93f460284a86aa2dd02faa624cf164fa1a03
Hisayori Noda <nodchip@gmail.com>
2016-01-22 08:14:53 +0900

    詰みを発見したら即指すようにした Fail-low/high以外の評価値は必ず出力するようにした

--- src/search.cpp
+++ src/search.cpp
@@ -64,6 +64,8 @@ namespace {
   // info を標準出力へ出力するスロットル
   // 前回出力してから以下の時間を経過していない場合は出力しない
   static constexpr int THROTTLE_TO_OUTPUT_INFO_MS = 200;
+  // Fail-low/highでない評価値を必ず出力する経過時間
+  static constexpr int OUTPUT_COMPLETE_SCORE_FROM_MS = 1000;
   static constexpr Score INITIAL_ASPIRATION_WINDOW_WIDTH = (Score)16;
   static constexpr Score SECOND_ASPIRATION_WINDOW_WIDTH = (Score)64;
   // true にすると、シングルスレッドで動作する。デバッグ用。
@@ -638,10 +640,10 @@ void Searcher::idLoop(Position& pos) {
           rootMoves[i].insertPvInTT(pos);
         }
 
-#if 0
+#if 1
         // 詰みを発見したら即指す。
         if (ScoreMateInMaxPly <= abs(bestScore) && abs(bestScore) < ScoreInfinite) {
-          SYNCCOUT << pvInfoToUSI(pos, ply, alpha, beta) << SYNCENDL;
+          SYNCCOUT << pvInfoToUSI(pos, depth, alpha, beta) << SYNCENDL;
           signals.stop = true;
         }
 #endif
@@ -650,7 +652,9 @@ void Searcher::idLoop(Position& pos) {
         break;
 #endif
 
-        if (lastTimeToOutputInfoMs + THROTTLE_TO_OUTPUT_INFO_MS < searchTimer.elapsed()) {
+        if (lastTimeToOutputInfoMs + THROTTLE_TO_OUTPUT_INFO_MS < searchTimer.elapsed() ||
+          (OUTPUT_COMPLETE_SCORE_FROM_MS < searchTimer.elapsed() &&
+            (alpha < bestScore && bestScore < beta))) {
           if (outputInfo) {
             SYNCCOUT << pvInfoToUSI(pos, depth, alpha, beta) << SYNCENDL;
           }
@@ -769,7 +773,7 @@ void Searcher::detectInaniwa(const Position& pos) {
       inaniwaFlag = (pos.turn() == Black ? InaniwaIsWhite : InaniwaIsBlack);
       tt.clear();
     }
-  }
+}
 }
 #endif
 #if defined BISHOP_IN_DANGER
@@ -1190,9 +1194,9 @@ split_point_start:
         SYNCCOUT << "info depth " << depth / OnePly
           << " currmove " << move.toUSI()
           << " currmovenumber " << moveCount + pvIdx << SYNCENDL;
-      }
+  }
 #endif
-    }
+}
 
     extension = Depth0;
     captureOrPawnPromotion = move.isCaptureOrPawnPromotion();
@@ -1369,14 +1373,14 @@ split_point_start:
           || (bishopInDangerFlag == WhiteBishopInDangerIn78 && move.toCSA() == "0078KA"))
         {
           rm.score_ -= options[OptionNames::DANGER_DEMERIT_SCORE];
-        }
+      }
 #endif
         rm.extractPvFromTT(pos);
 
         if (!isPVMove) {
           ++bestMoveChanges;
         }
-      }
+    }
       else {
         rm.score_ = -ScoreInfinite;
       }
