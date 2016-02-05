42c79ec757206996412543cf3d2a60fa331ed27c
Hisayori Noda <nodchip@gmail.com>
2016-02-05 21:45:39 +0900

    1対1のプロキシを実装した

--- tanuki-/tanuki-proxy/Program.cs
+++ tanuki-/tanuki-proxy/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace tanuki_proxy
 {
@@ -10,6 +11,47 @@ namespace tanuki_proxy
     {
         static void Main(string[] args)
         {
+            // 子プロセスの標準入出力 (System.Diagnostics.Process) - Programming/.NET Framework/標準入出力 - 総武ソフトウェア推進所 http://smdn.jp/programming/netfx/standard_streams/1_process/
+            using (Process child = new Process())
+            {
+                child.StartInfo.FileName = "C:\\home\\develop\\tanuki-\\tanuki-\\x64\\Release\\tanuki-.exe";
+                child.StartInfo.WorkingDirectory = "C:\\home\\develop\\tanuki-\\bin";
+                child.StartInfo.UseShellExecute = false;
+                child.StartInfo.RedirectStandardInput = true;
+                child.StartInfo.RedirectStandardOutput = true;
+                child.OutputDataReceived += HandleStdout;
+
+                if (!child.Start())
+                {
+                    return;
+                }
+
+                child.BeginOutputReadLine();
+
+                string line;
+                while ((line = Console.ReadLine()) != null)
+                {
+                    child.StandardInput.WriteLine(line);
+                    child.StandardInput.Flush();
+
+                    if (line == "quit")
+                    {
+                        break;
+                    }
+                }
+                child.StandardInput.Close();
+                child.WaitForExit();
+            }
+        }
+
+        private static void HandleStdout(object sender, DataReceivedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.Data))
+            {
+                return;
+            }
+
+            Console.WriteLine(e.Data);
         }
     }
 }
