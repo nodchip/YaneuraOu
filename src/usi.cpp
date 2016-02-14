64bab2788bda4f2cc38a2ee8b46eeb7496d6ca6e
Hisayori Noda <nodchip@gmail.com>
2016-02-14 18:48:26 +0900

    stopコマンド受信時に思考スレッドを待機するようにした

--- src/usi.cpp
+++ src/usi.cpp
@@ -469,6 +469,7 @@ void Searcher::doUSICommandLoop(int argc, char* argv[]) {
       if (token != "ponderhit" || signals.stopOnPonderHit) {
         signals.stop = true;
         threads.mainThread()->notifyOne();
+        threads.waitForThinkFinished();
       }
       else {
         limits.ponder = false;
