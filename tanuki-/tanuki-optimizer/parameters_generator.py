f9d6a76e971798a87fc26b8b9dd4325a0c489105
Hisayori Noda <nodchip@gmail.com>
2016-03-02 22:55:54 +0900

    パラメーターの順序を修正した

--- tanuki-/tanuki-optimizer/parameters_generator.py
+++ tanuki-/tanuki-optimizer/parameters_generator.py
@@ -10,8 +10,8 @@ def main():
             name, default_value, min_value, max_value = row
             #print("{0}={1}".format(name, default_value))
             #print('-D{0}=$({0})'.format(name))
-            #print("hp.quniform('{0}', {1}, {2}),".format(name, min_value, max_value))
-            print("		'{0}=' + args[{1}],".format(name, index))
+            #print("  hp.quniform('{0}', {1}, {2}, 1),".format(name, min_value, max_value))
+            print("    '{0}=' + str(int(args[{1}])),".format(name, index))
 
 if __name__ == '__main__':
     main()
