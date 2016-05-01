034c264be99c630a73549afcfdc316945f9fc6be
nodchip <nodchip@gmail.com>
2016-05-01 15:56:00 +0900

    定跡データベースヒット時に1.5秒待機するようにした

--- src/search.cpp
+++ src/search.cpp
@@ -216,6 +216,9 @@ void MainThread::search() {
           << " pv " << move.toUSI()
           << SYNCENDL;
       }
+
+      std::chrono::milliseconds dura(1500);
+      std::this_thread::sleep_for(dura);
     }
     else {
       for (Thread* th : Threads)
