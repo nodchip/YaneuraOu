3ea51abbf2b94b7711f07a20eeb09ccdee601aae
Hisayori Noda <nodchip@gmail.com>
2017-05-13 09:48:03 +0900

    パラメーターセットを更新した

--- tanuki-optimizer/parameters_generator.py
+++ tanuki-optimizer/parameters_generator.py
@@ -5,7 +5,7 @@ import csv
 import math
 
 def main():
-  with open('param/parameters.opening.csv', 'r') as f:
+  with open('parameters.csv', 'r') as f:
     reader = csv.reader(f)
     for index, row in enumerate(reader):
       name, default_value, min_value, max_value = row
@@ -24,7 +24,7 @@ def main():
       #print('#define {0} {1}'.format(name, str(int(parameters[index]))))
       #print('#endif')
       #print("{0}={1}".format(name, str(int(parameters[index]))))
-      #print("  '{0}',".format(name))
+      print("  '{0}',".format(name))
       #sigma = float(default_value) / 10.0 / 3.0
       #print("  hp.qnormal('{0}', {1}, {2}, 1),".format(name, default_value, sigma))
       #print("  '{0}',".format(name))
@@ -34,9 +34,9 @@ def main():
       #print("  '{name}': [0.0, 1.0],".format(name=name))
       #print("  print('{name:50s}= {{{name}}}'.format({name}={name}))".format(name=name))
       #print('PARAM_DEFINE {name} = {default_value};'.format(name=name, default_value=default_value))
-      min_value = max(min_value, int(math.floor(float(default_value) * 0.75)))
-      max_value = min(max_value, int(math.ceil(float(default_value) * 1.25)))
-      print("  hp.quniform('{0}', {1}, {2}, 1),".format(name, min_value, max_value))
+      #min_value = max(min_value, int(math.floor(float(default_value) * 0.75)))
+      #max_value = min(max_value, int(math.ceil(float(default_value) * 1.25)))
+      #print("  hp.quniform('{0}', {1}, {2}, 1),".format(name, min_value, max_value))
 
 
 if __name__ == '__main__':
